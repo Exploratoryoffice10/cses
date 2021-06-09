9c973490b8eeb34e674d4d916540391920739de3
abhi <abhi.ahv@gmail.com>
2021-06-09 23:04:36 +0530

    completed grid paths

--- IP/gp.cpp
+++ IP/gp.cpp
@@ -1,3 +1,5 @@
+// CSES GRID PATHS
+
 #include <iostream>
 
 using namespace std;
@@ -24,22 +26,27 @@ string s;
 ll ans=0,n=8;
 int vis[7][7]={0};
 
+bool freed(int i,int j){
+    return i>=0&&i<7&&j>=0&&j<7&&!vis[i][j];
+}
 
 void dfs(int i,int j,int nm=0)	// nm is number of moves made
 {
-	vis[i][j]=1;	
-	if(i==6 && j==6)
+//     cout << i<<" "<<j<< " " <<nm<<endl;
+	if(i==6 && j==0)
 	{
 		if(nm==48)
 		{
 			ans++;
 		}
 		return;
-	}		
+	}
+	vis[i][j]=1;	
 	if(s[nm]=='?' || s[nm]=='R')
 	{
 		if(j<6 && !vis[i][j+1])
 		{
+            if(!(!freed(i,j+2)&&freed(i-1,j+1)&&freed(i+1,j+1)))
 			dfs(i,j+1,nm+1);
 		}
 	}
@@ -47,27 +54,26 @@ void dfs(int i,int j,int nm=0)	// nm is number of moves made
 	if(s[nm]=='?' || s[nm]=='L'){
 		if(j && !vis[i][j-1])
 		{
+            if(!(!freed(i,j-2)&&freed(i-1,j-1)&&freed(i+1,j-1)))
 			dfs(i,j-1,nm+1);
 		}
 	}
-	
 	if(s[nm]=='?' || s[nm]=='U')
 	{
 		if(i && !vis[i-1][j])
 		{
-			dfs(i-1,j,nm+1);
+            if(!(!freed(i-2,j)&&freed(i-1,j+1)&&freed(i-1,j-1)))
+            dfs(i-1,j,nm+1);
 		}
 	}
-	
 	if(s[nm]=='?' || s[nm]=='D'){
 		if(i<6 && !vis[i+1][j])
 		{
+            if(!(!freed(i+2,j)&&freed(i+1,j+1)&&freed(i+1,j-1)))
 			dfs(i+1,j,nm+1);
 		}
 	}
-
 	vis[i][j]=0;
-	return;
 }
 
 
